6179d89b0b69e62b1edcd8e104574631105b6683
Hà Thiên Vân <hathienvan@MacBook-Pro-cua-Ha.local>
2019-11-12 15:15:56 +0700

    11

--- AppChatJetArt/app/src/main/java/hathienvan/firstapplication/appchatjetart/SettingActivity.java
+++ AppChatJetArt/app/src/main/java/hathienvan/firstapplication/appchatjetart/SettingActivity.java
@@ -51,8 +51,8 @@ public class SettingActivity extends AppCompatActivity {
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                User user = dataSnapshot.getValue(User.class);
-                username.setText(user.getUsername());
+//                User user = dataSnapshot.getValue(User.class);
+                //                username.setText(user.getUsername());
 //                if (user.getImageURL().equals("default")){
 //                    circleImageView.setImageResource(R.drawable.oval_blue);
 //                }else {
